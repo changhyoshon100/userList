cbf3693a1b6660b386b19f5c620becba8fe0a090
changhyoshon <changhyoshon@changhyos-MBP.hsd1.ga.comcast.net>
2023-10-08 18:29:07 -0400

    Initial commit

---
+++ src/components/UI/ErrorModal.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import Card from './Card';
+import Button from './Button';
+import classes from './ErrorModal.module.css';
+
+const Backdrop = (props) => {
+  return(
+    <div className={classes.backdrop} onClick={props.onConfirm} />
+  )
+}
+
+const Modal = (props) => {
+  return (
+      <Card className={classes.modal}>
+        <header className={classes.header}>
+          <h2>{props.title}</h2>
+        </header>
+        <div className={classes.content}>
+          <p>{props.message}</p>
+        </div>
+        <footer className={classes.actions}>
+          <Button onClick={props.onConfirm}>Okay</Button>
+        </footer>
+      </Card>
+  )
+}
+
+const ErrorModal = (props) => {
+  return (
+    <React.Fragment>
+      <Backdrop onConfirm={props.onConfirm}/>
+      <Modal 
+        title={props.title}
+        message={props.message}
+        onConfirm={props.onConfirm}
+      />
+    </React.Fragment>
+  );
+};
+
+export default ErrorModal;
